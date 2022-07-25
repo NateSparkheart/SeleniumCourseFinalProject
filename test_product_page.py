30383a70f3b379c0afdb543145164b636dac5cfa
Nate <kappavlad25@gmail.com>
2022-07-25 04:48:06 +0300

    Методы MainPage перенесены в BasePage, добавлены тесты для страницы продукта

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,18 @@
 from .pages.product_page import ProductPage
 import pytest
 
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-girl-who-played-with-non-fire_203/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
 @pytest.mark.parametrize('offer_num', [*range(7), pytest.param(7, marks=pytest.mark.xfail
     (reason="Некорректное название в сообщении о добавлении в корзину, не чиним")), *range(8,10)])
 def test_guest_can_add_product_to_basket(browser, offer_num):
--- test_product_page_2.py
+++ test_product_page_2.py
@@ -1,7 +1,9 @@
 from .pages.product_page import ProductPage
+import pytest
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
+@pytest.mark.xfail(reason="Всё окей, сообщение должно быть")
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
@@ -13,6 +15,7 @@ def test_guest_cant_see_success_message(browser):
     page.open()
     page.should_not_be_success_message()
 
+@pytest.mark.xfail(reason="Сообщение не пропадает, фиксить не будем до повышения ЗП на 300%")
 def test_message_disappeared_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
